4e21951deccc5daabf3a6e119470fe4b7d5cbcf9
SyedArsalanAmin <sarsalanamin12@gmail.com>
2021-03-13 00:17:12 +0500

    making prediction

--- Prediction using Supervised ML- Task 1.py
+++ Prediction using Supervised ML- Task 1.py
@@ -9,18 +9,28 @@ sns.set()
 
 # loading dataset
 dataset = pd.read_csv(
-    "E:\DataScience & AI\Github_repo\datasets\student_scores - student_scores.csv", delimiter=",")
+    "E:\\DataScience & AI\\Github_repo\\datasets\\student_scores - student_scores.csv", delimiter=",")
 
+# Visualizing and exploring data
 dataset.shape
-
+dataset.describe()
 sns.pairplot(dataset)  # to see correlation between the two features
 plt.scatter(dataset["Hours"], dataset["Scores"], c='r')
 plt.xlabel("Study Hours")
 plt.ylabel("Percentage Obtained")
 plt.title("Percentage vs Hours Studied")
 
-dataset.head()
-X = dataset.iloc[:, 0].values
-y = dataset.iloc[:, 1].values
 
-X_train, X_test, y_taintrain_test_split(X, y, test_size=0.2)
+dataset.shape
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+X.shape
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2)  # splitting in train and test data
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+model.predict(X_test)
+model.predict([[9.25]])
+# -------------------------------------------------------------------------------
