a60f5e0c069459d52235612f2d730fee39b2491e
SyedArsalanAmin <sarsalanamin12@gmail.com>
2021-03-14 16:48:06 +0500

    Updating with petals clusters

--- Prediction using Unsupervised ML-Task-2.py
+++ Prediction using Unsupervised ML-Task-2.py
@@ -21,7 +21,8 @@ def scatter_plot(dataset, col1, col2):
     plt.title("Petal anaylysis")
 
 
-scatter_plot(df, 2, 3)
+scatter_plot(df, 2, 3)  # visualizing petal data
+scatter_plot(df, 0, 1)  # visualizing sepal data
 
 scaler = MinMaxScaler()
 scaled_features = scaler.fit_transform(df)
@@ -32,24 +33,79 @@ scaled_features[:3]  # these are the normalized feature set between 0-1
 kmeans = KMeans(n_clusters=3)
 y_pred = kmeans.fit_predict(scaled_features)
 y_pred  # so thses are the predicted categories of the data we provided to kmeans
+
+df["SepalLengthCm"] = scaled_features[:, 0]
+df["SepalWidthCm"] = scaled_features[:, 1]
+df["PetalLengthCm"] = scaled_features[:, 2]
+df["PetalWidthCm"] = scaled_features[:, 3]
 df["Clusters"] = y_pred
-df.head()
+
+
+scaled_features[:5]
+df.head()  # Normalized dataset
 df.describe()  # looking into the data for insights
 # for a better understadin of the data lets take a look at the correlation between different features
 sns.pairplot(df)
 
 
-# Making  Clusters
-cluster1 = df[df['Clusters'] == 0].reset_index()
-cluster1.head(3)
-cluster2 = df[df['Clusters'] == 1].reset_index()
-cluster2.head(3)
-cluster3 = df[df['Clusters'] == 2].reset_index()
-cluster3.head(3)
+# Making Petal Clusters
+pet_cluster1 = df[df['Clusters'] == 0].reset_index(drop=True)
+pet_cluster1.head(3)
+pet_cluster2 = df[df['Clusters'] == 1].reset_index(drop=True)
+pet_cluster2.head(3)
+pet_cluster3 = df[df['Clusters'] == 2].reset_index(drop=True)
+pet_cluster3.head(3)
 
-# Cluster centers
-centers = kmeans.cluster_centers_[:3, -2:]  # cluster center for petals
+# Making Sepal Clusters
+sep_cluster1 = df[df['Clusters'] == 0].reset_index(drop=True)
+sep_cluster1.head(3)
+sep_cluster2 = df[df['Clusters'] == 1].reset_index(drop=True)
+sep_cluster2.head(3)
+sep_cluster3 = df[df['Clusters'] == 2].reset_index(drop=True)
+sep_cluster3.head(3)
+
+# Plotting clusters
+
+
+def plot_sep_cluster():
+    plt.scatter(sep_cluster1.iloc[:, 2], sep_cluster1.iloc[:, 3], c='r',
+                marker='o', edgecolors='black', label="Cluster-1")
+    plt.scatter(sep_cluster2.iloc[:, 2], sep_cluster2.iloc[:, 3], c='b',
+                marker='v', edgecolors='black', label="Cluster-2")
+    plt.scatter(sep_cluster3.iloc[:, 2], sep_cluster3.iloc[:, 3], c='y',
+                marker='s', edgecolors='black', label="Cluster-3")
+    plt.legend
+
+    centers = kmeans.cluster_centers_[:, -2:]  # cluster center for petals
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', marker='X', s=200, label="Centroids")
+    plt.figure(figsize=(12, 12))
+    plt.show()
+
+
+plot_sep_cluster()
+
+
+def plot_pet_cluster():
+    plt.scatter(pet_cluster1.iloc[:, 0], pet_cluster1.iloc[:, 1], c='r',
+                marker='o', edgecolors='black', label="Cluster-1")
+    plt.scatter(pet_cluster2.iloc[:, 0], pet_cluster2.iloc[:, 1], c='b',
+                marker='v', edgecolors='black', label="Cluster-2")
+    plt.scatter(pet_cluster3.iloc[:, 0], pet_cluster3.iloc[:, 1], c='y',
+                marker='s', edgecolors='black', label="Cluster-3")
+    plt.legend
+
+    centers = kmeans.cluster_centers_[:, :-2]  # cluster center for petals
+    centers
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', marker='X', s=200, label="Centroids")
+    plt.figure(figsize=(12, 12))
+    plt.show()
+
+
+plot_pet_cluster()
+
+
+# -----------------------------------------------------------------------------
+sep_cluster1
+centers = kmeans.cluster_centers_[:, :-2]
 centers
-plt.scatter(centers[0, 0], centers[0, 1], c='r')
-plt.scatter(centers[1, 0], centers[1, 1], c='b')
-plt.scatter(centers[2, 0], centers[2, 1], c='y')
+centers[:, 1]
